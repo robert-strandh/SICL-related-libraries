fc271e87d37a40be144863f191d71769cff47928
Robert Strandh <robert.strandh@gmail.com>
2025-04-25 08:15:31 +0200

    Add file for ctype.

---
+++ ctype.lisp
@@ -0,0 +1,53 @@
+<HTML>
+<HEAD>
+<TITLE>ctype: An implemenation of the Common Lisp type system</TITLE>
+<LINK rel="stylesheet" type="text/css" href="mm.css">
+</HEAD>
+<BODY>
+<H1>ctype: An implementation of the Common Lisp type system</H1>
+
+<H2>Description</H2>
+
+<p>
+
+  This library provides implementations of the Common Lisp type
+  system, and in particular of the two essential functions
+  <tt>typep</tt> and <tt>subtypep</tt>.  It works by canonicalizing
+  type specifiers and representing them as instances of standard
+  classes.
+
+</p>
+
+
+<H2>Requirements</H2>
+
+<p>
+
+  This library does not depend on any other system, but CLOS is
+  required.
+
+</p>
+
+
+<H2>Maintainers</H2>
+
+<p>
+
+  Currently maintained by Alex Wood.
+
+</p>
+
+<H2>Possible improvements</H2>
+
+<p>
+
+  Documentation strings for remaining functions.  A test suite. 
+
+</p>
+
+<HR>
+<address>
+robert.strandh@gmail.com
+</address>
+</BODY>
+</HTML>
